50d53cdac94da6a5db41a733428c2cf1412b1e60
snsaleh1 <snsaleh11@gmail.com>
2019-11-12 15:59:29 -0600

    max character solution

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,6 +7,16 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+function palindrome(str) {
+    let reversed = str
+    .split('')
+    .reverse()
+    .join('');
+
+    if(reversed === str){
+        return true;
+    }
+        return false;
+}
 
 module.exports = palindrome;
