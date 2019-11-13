3d352204ec8b5c59032d78953901bc0b1bac45c1
snsaleh1 <snsaleh11@gmail.com>
2019-11-12 22:47:25 -0600

    steps solution

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,28 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+function steps(n) {
+    //from 0 to n (iterate through ROWS)
+    for (let row = 0; row < n; row++) {
+        //create an empty string 'stair'
+        let stair = '';
+        //from 0 to n (iterate through COLUMNS)
+        for (let column = 0; column < n; column++) {
+            //if the current column is equal to or less than the current row
+            if(column <= row) {
+                //add a # to 'stair'
+                stair += '#';
+                //else
+            } else {
+                //add a space to 'stair'
+                stair += ' ';
+            }
+        }
+        //console.log(stair)
+        console.log(stair);
+    }
+}
+
+
 
 module.exports = steps;
