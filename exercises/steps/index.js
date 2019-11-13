2d8d7bc970699e86208ef2c878be642f4c692b1e
snsaleh1 <snsaleh11@gmail.com>
2019-11-12 23:03:39 -0600

    stair solution using recursion

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,26 +17,47 @@
 //       '### '
 //       '####'
 
-function steps(n) {
-    //from 0 to n (iterate through ROWS)
-    for (let row = 0; row < n; row++) {
-        //create an empty string 'stair'
-        let stair = '';
-        //from 0 to n (iterate through COLUMNS)
-        for (let column = 0; column < n; column++) {
-            //if the current column is equal to or less than the current row
-            if(column <= row) {
-                //add a # to 'stair'
-                stair += '#';
-                //else
-            } else {
-                //add a space to 'stair'
-                stair += ' ';
-            }
-        }
-        //console.log(stair)
-        console.log(stair);
+// function steps(n) {
+//     //from 0 to n (iterate through ROWS)
+//     for (let row = 0; row < n; row++) {
+//         //create an empty string 'stair'
+//         let stair = '';
+//         //from 0 to n (iterate through COLUMNS)
+//         for (let column = 0; column < n; column++) {
+//             //if the current column is equal to or less than the current row
+//             if(column <= row) {
+//                 //add a # to 'stair'
+//                 stair += '#';
+//                 //else
+//             } else {
+//                 //add a space to 'stair'
+//                 stair += ' ';
+//             }
+//         }
+//         //console.log(stair)
+//         console.log(stair);
+//     }
+// }
+function steps(n, row = 0, stair = '') {
+    //if (row === n) then we have hit the end of the problem
+    if (n === row) {
+        return;
     }
+    //if the 'stair' has a length === n then we are at the end of a row
+    if (n === stair.length) {
+        console.log(stair);
+        steps(n, row + 1);
+        return;
+    }
+    //if the length of the stair string 
+    //is less than or equal to the row we are working on, 
+    //we add a #, otherwise we add a ' '
+    if (stair.length <= row) {
+        stair += '#';
+    } else {
+        stair += ' ';
+    }
+    steps(n, row, stair);
 }
 
 
