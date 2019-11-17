7c63a71ad09c263005691732dbeb45b31d292c63
snsaleh1 <snsaleh11@gmail.com>
2019-11-16 23:08:37 -0600

    pyramid solution 1 set

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -6,20 +6,20 @@
 // maxChar("apple 1231111") === "1"
 
 function maxChar(str) {
-    const charMap={};
+    const charMap = {};
     let max = 0;
     let maxChar = '';
 
     for (let char of str) {
-        if (charMap[char]){
+        if (charMap[char]) {
             charMap[char]++;
         } else {
-            charMap[char]=1;
+            charMap[char] = 1;
         }
     }
 
-    for (let char in charMap){
-        if(charMap[char] > max){
+    for (let char in charMap) {
+        if (charMap[char] > max) {
             max = charMap[char];
             maxChar = char;
         }
@@ -29,3 +29,29 @@ function maxChar(str) {
 }
 
 module.exports = maxChar;
+
+
+/* 	function mode(numbers) {
+    // as result can be bimodal or multi-modal,
+    // the returned result is provided as an array
+    // mode of [3, 5, 4, 4, 1, 1, 2, 3] = [1, 3, 4]
+    var modes = [], count = [], i, number, maxIndex = 0;
+ 
+    for (i = 0; i < numbers.length; i += 1) {
+        number = numbers[i];
+        count[number] = (count[number] || 0) + 1;
+        if (count[number] > maxIndex) {
+            maxIndex = count[number];
+        }
+    }
+ 
+    for (i in count)
+        if (count.hasOwnProperty(i)) {
+            if (count[i] === maxIndex) {
+                modes.push(Number(i));
+            }
+        }
+ 
+    return modes;
+}
+ */
--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,6 +14,8 @@
 //       ' ### '
 //       '#####'
 
+//This is the iterative solution
+
 function pyramid(n) {
     const midpoint = Math.floor((2*n - 1) / 2);
     for (let row = 0; row < n; row++) {
